a4c58ee4fb5d90cd5c2c9a4208ebc1b2b134e1df
stephane-klein <contact@stephane-klein.info>
2024-08-05 13:49:40 +0200

    Add pagination navigation buttons in root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,19 @@
     export let data;
 </script>
 
-<p>Recent notes:</p>
-
+{#if (data.countNewNotes === 0)}
+    Notes les plus récentes :
+{:else}
+    <p style="text-align: center">
+        [ <a href={`?created_after=${format(data.firstNote.created_at, "yyyyMMddHHmmss")}`}>&lt;&lt; Notes plus récentes
+            ({data.countNewNotes})</a> ]
+            {#if (data.countOldNotes === 0)}
+                Pas de notes plus anciennes
+            {:else}
+                [ <a href={`?created_before=${format(data.lastNote.created_at, "yyyyMMddHHmmss")}`}>Notes plus anciennes ({data.countOldNotes}) &gt;&gt; </a> ]
+            {/if}
+    </p>
+{/if}
 {#each Object.entries(data.notesByDay) as [date, notes]}
     <h2>{date}</h2>
     {#each notes as note}
@@ -18,7 +29,19 @@
         <hr />
     {/each}
 {/each}
-<p style="text-align: center">
-    [ <a href={`?created_after=${format(data.firstNote.created_at, "yyyyMMddHHmmss")}`}>&lt;&lt; Notes plus récentes</a> ] |
-    [ <a href={`?created_before=${format(data.lastNote.created_at, "yyyyMMddHHmmss")}`}>Notes plus anciennes &gt;&gt;</a> ]
-</p>
+{#if (data.countOldNotes === 0)}
+    <p style="text-align: center">
+        Fin de la liste des notes.
+    </p>
+{:else}
+    <p style="text-align: center">
+        {#if (data.countNewNotes === 0)}
+            Pas de notes plus récentes
+        {:else}
+        [ <a href={`?created_after=${format(data.firstNote.created_at, "yyyyMMddHHmmss")}`}>&lt;&lt; Notes plus récentes
+            ({data.countNewNotes})</a> ]
+        {/if}
+        |
+        [ <a href={`?created_before=${format(data.lastNote.created_at, "yyyyMMddHHmmss")}`}>Notes plus anciennes ({data.countOldNotes}) &gt;&gt; </a> ]
+    </p>
+{/if}
