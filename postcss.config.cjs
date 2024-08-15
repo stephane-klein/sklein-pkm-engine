aa9b328b9a40627cce77fd096da1a2b9c71e71c0
stephane-klein <contact@stephane-klein.info>
2024-08-15 16:38:49 +0200

    Setup postcss

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    map: true,
+    plugins: {
+        'postcss-normalize': {},
+        'postcss-nested': {},
+        'autoprefixer': {}
+    }
+};
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,12 @@
 import adapter from "@sveltejs/adapter-node";
+import preprocess from "svelte-preprocess";
 
 const config = {
+    preprocess:[
+        preprocess({
+            postcss: true
+        })
+    ],
 	kit: {
 		adapter: adapter()
 	}
