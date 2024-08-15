472569a9984eb595285f0d65c58a09658ebec501
stephane-klein <contact@stephane-klein.info>
2024-08-15 16:39:13 +0200

    Add /tags/ page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,14 @@
     import "highlight.js/styles/github.css";
 </script>
 
+<nav style="margin: 1em 0;">
+    <ul
+        style="list-style: none; padding: 0; margin: 1em 0; font-size: 1em; display: flex; gap: 0.5em; flex-wrap: wrap;"
+    >
+        <li><a href="/">Accueil</a> | </li>
+        <li><a href="/search/">Recherche</a> | </li>
+        <li><a href="/tags/">Explorer tous les tags</a></li>
+    </ul>
+</nav>
+
 <slot />
---
+++ src/routes/tags/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let data;
+</script>
+
+<ul class="search-tags-panel">
+    {#each data.tags as tag}
+        <li><a
+            href={`/search/?tags=${tag.name}`}
+            >{tag.name} ({tag.note_counts})</a>
+        </li>
+    {/each}
+</ul>
+
+<style>
+    .search-tags-panel {
+        margin: 1em 0;
+        font-size: 0.7em;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        column-gap: 0.5em;
+        row-gap: 1em;
+        flex-wrap: wrap;
+
+        > LI {
+            display: inline-block;
+            padding: 0.2em 0.4em;
+            border: 1px solid #aaa;
+
+            > A {
+                white-space: nowrap;
+                text-decoration: none;
+            }
+        }
+    }
+</style>
