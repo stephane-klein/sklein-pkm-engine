2066fbe6592b435858c09e539a72fdbe664ed87e
stephane-klein <contact@stephane-klein.info>
2024-08-19 21:57:21 +0200

    WIP

--- src/routes/[note_filename]/+page.server.js
+++ src/routes/[note_filename]/+page.server.js
@@ -1,66 +1,52 @@
 import esClient from "$lib/server/elasticsearch.js";
 
 export async function load({params}) {
-    const note = (await esClient.search({
-        index: "notes",
-        body: {
-            _source: ["title", "note_type", "content_html", "tags"],
-            query: {
-                bool: {
-                    filter: [
-                        {
-                            terms: {
-                                filename: [params.note_filename]
-                            }
-                        },
-                    ]
-                }
-            },
-        }
-    })).hits.hits[0];
-
-    const { hits: backlink_notes} = (await esClient.search({
-        index: "notes",
-        body: {
-            _source: ["title", "filename", "note_type", "content_html", "tags", "created_at"],
-            query: {
-                bool: {
-                    filter: [
-                        {
-                            terms: {
-                                linked_notes: [params.note_filename]
-                            }
-                        },
-                    ]
-                }
-            },
-            sort: [
-                {
-                    created_at: {
-                        order: "desc"
+    const [noteResult, backlinkNotesResult] = await Promise.all([
+        esClient.search({
+            index: "notes",
+            body: {
+                _source: ["title", "note_type", "content_html", "tags"],
+                query: {
+                    bool: {
+                        filter: [
+                            {
+                                terms: {
+                                    filename: [params.note_filename]
+                                }
+                            },
+                        ]
                     }
-                }
-            ]
-        }
-    })).hits;
-
-    console.dir(backlink_notes, {depth: null});
-
-    /*
-    if (note) {
-        data.backlink_notes = data.backlink_notes.map((note) => {
-            return {
-                html: md.render(note?.content || ""),
-                ...note
-            };
-        });
-    } else {
-        error(404, 'Not Found');
-    }
-    */
+                },
+            }
+        }),
+        esClient.search({
+            index: "notes",
+            body: {
+                _source: ["title", "filename", "note_type", "content_html", "tags", "created_at"],
+                query: {
+                    bool: {
+                        filter: [
+                            {
+                                terms: {
+                                    linked_notes: [params.note_filename]
+                                }
+                            },
+                        ]
+                    }
+                },
+                sort: [
+                    {
+                        created_at: {
+                            order: "desc"
+                        }
+                    }
+                ]
+            }
+        })
+    ]);
 
     return {
-        note,
-        backlink_notes
+        note: noteResult.hits.hits[0],
+        backlink_notes: backlinkNotesResult.hits.hits
     };
 }
