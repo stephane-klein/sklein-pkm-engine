abdfba798f6ad4a2edc3116d2aaaa26718fc6aa5
stephane-klein <contact@stephane-klein.info>
2024-08-24 21:34:06 +0200

    WIP

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,12 @@
+import esClient from "$lib/server/elasticsearch.js";
 import search from "./search/search.js";
 
 export async function load({url}) {
+    if (!await esClient.indices.exists({ index: "notes" })) {
+        return {
+            databaseNotConfigured: true
+        };
+    }
     return search({
         createdAfter: url.searchParams.get("created_after"),
         createdBefore: url.searchParams.get("created_before")
