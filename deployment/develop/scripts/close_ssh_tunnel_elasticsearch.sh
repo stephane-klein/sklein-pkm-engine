7313460e823312635bc78efb31c6f5e6808d45db
stephane-klein <contact@stephane-klein.info>
2024-08-25 18:24:47 +0200

    Fixed renaming omissions following migration from postgres to elasticsearch

--- deployment/develop/scripts/close_ssh_tunnel_elasticsearch.sh
+++ deployment/develop/scripts/close_ssh_tunnel_elasticsearch.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-PID_FILE="open_ssh_tunnel_postgres.pid"
+PID_FILE="open_ssh_tunnel_elasticsearch.pid"
 
 if [ -f ${PID_FILE} ]; then
     SSH_PID=$(cat ${PID_FILE})
@@ -12,11 +12,11 @@ if [ -f ${PID_FILE} ]; then
     kill ${SSH_PID} 2>/dev/null
 
     if [ $? -eq 0 ]; then
-        echo "SSH tunnel postgres closed with success"
+        echo "SSH tunnel elasticsearch closed with success"
         rm -f ${PID_FILE}
     else
         rm -f ${PID_FILE}
-        echo "Error, no ssh tunnel was open, the open_ssh_tunnel_postgres.pid file has been deleted to correct this error."
+        echo "Error, no ssh tunnel was open, the open_ssh_tunnel_elasticsearch.pid file has been deleted to correct this error."
     fi
 else
     echo "No open ssh tunnel found"
