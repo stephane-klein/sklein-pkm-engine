dda33fcadff366aa0dd3639550e860cd84ba251e
stephane-klein <contact@stephane-klein.info>
2024-08-25 19:25:06 +0200

    Add RemoveTag feature in search page

---
+++ src/routes/search/RemoveTag.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let currentUrl;
+    export let tag;
+    import TablerX from "~icons/tabler/x";
+
+    let href;
+    $: {
+        const url = new URL(currentUrl);
+        url.searchParams.delete('tags', tag.key);
+        href = url.toString();
+    }
+</script>
+<a
+    href={href}
+    >{tag.key}
+{#if tag.doc_count}
+    ({tag.doc_count})
+{/if}
+    <TablerX
+        width="1em"
+        height="1em"
+        style="vertical-align: middle"
+    />
+</a>
+<style>
+    a {
+        white-space: nowrap;
+        text-decoration: none;
+    }
+</style>
