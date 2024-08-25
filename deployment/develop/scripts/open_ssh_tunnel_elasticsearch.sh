e6c5cef5a915a77102d1ecf010e1ad3ede02838b
stephane-klein <contact@stephane-klein.info>
2024-08-25 18:24:47 +0200

    Fixed renaming omissions following migration from postgres to elasticsearch

--- deployment/develop/scripts/open_ssh_tunnel_elasticsearch.sh
+++ deployment/develop/scripts/open_ssh_tunnel_elasticsearch.sh
@@ -3,16 +3,16 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-if [ -e "open_ssh_tunnel_postgres.pid" ]; then
+if [ -e "open_ssh_tunnel_elasticsearch.pid" ]; then
     cat <<EOF
-There's an open_ssh_tunnel_postgres.pid file, which means that either a tunnel has already opened or a tunnel has been closed incorrectly.
-To fix this problem, run "./scripts/close_ssh_tunnel_postgres.sh".
+There's an open_ssh_tunnel_elasticsearch.pid file, which means that either a tunnel has already opened or a tunnel has been closed incorrectly.
+To fix this problem, run "./scripts/close_ssh_tunnel_elasticsearch.sh".
 EOF
     exit 1
 fi
 
 if [ -z "${INSTANCE_DEVELOP_ELASTICSEARCH_PORT}" ]; then
-    echo "Error : INSTANCE_DEVELOP_POSTGRES_PORT environment variable is not defined or empty"
+    echo "Error : INSTANCE_DEVELOP_ELASTICSEARCH_PORT environment variable is not defined or empty"
     exit 1
 fi
 
@@ -20,4 +20,4 @@ nohup ssh -L ${INSTANCE_DEVELOP_ELASTICSEARCH_PORT}:127.0.0.1:${INSTANCE_DEVELOP
 
 SSH_PID=$!
 
-echo ${SSH_PID} > open_ssh_tunnel_postgres.pid
+echo ${SSH_PID} > open_ssh_tunnel_elasticsearch.pid
