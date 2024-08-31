abe7a8edc0b0b37a5884777df805301d71a65909
stephane-klein <contact@stephane-klein.info>
2024-08-31 11:03:06 +0200

    Add /diaries/ page

---
+++ src/routes/diaries/+page.server.js
@@ -0,0 +1,169 @@
+import esClient from "$lib/server/elasticsearch.js";
+const notesByPage = 50;
+
+function groupByDay(notes) {
+    return notes.reduce((acc, note) => {
+        let date = note._source.created_at.substring(0, 10);
+
+        if (!acc[date]) {
+            acc[date] = [];
+        }
+
+        acc[date].push(note);
+
+        return acc;
+    }, {});
+}
+
+export async function load({url}) {
+    /* Build baseQuery used by all other queries */
+    const baseQuery = {
+        index: "notes",
+        body: {
+            query: {
+                bool: {
+                    must: [
+                        {
+                            term: {
+                                note_type: "journal_note"
+                            }
+                        },
+                    ]
+                }
+            }
+        }
+    };
+    const tags = url.searchParams.getAll("tags");
+
+    if (tags.length > 0) {
+        baseQuery.body.query.bool.must.push(
+            {
+                bool: {
+                    must: tags.map(tag => ({
+                        term: {
+                            tags: tag
+                        }
+                    }))
+                }
+            }
+        );
+    }
+
+    const createdAfter = url.searchParams.get("created_after");
+    const createdBefore = url.searchParams.get("created_before");
+
+    /* Build currentPageResultQuery */
+    const currentPageResultQuery = structuredClone(baseQuery);
+    currentPageResultQuery.body._source = ["title", "created_at", "filename", "content_html", "tags"];
+    currentPageResultQuery.body.size = notesByPage;
+
+    currentPageResultQuery.body.query.bool.must.push(
+        {
+            range: {
+                created_at: {
+                    lt: createdBefore || undefined,
+                    gt: createdAfter || undefined
+                }
+            }
+        }
+    );
+
+    currentPageResultQuery.body.sort = [
+        {
+            created_at: {
+                order: (
+                    (createdAfter !== null)
+                        ? "asc"
+                        : "desc"
+                )
+            }
+        }
+    ];
+
+    /* Build aggsResultQuery*/
+    const aggsResultQuery = structuredClone(baseQuery);
+    aggsResultQuery.body.aggs = {
+        tags_count: {
+            terms: {
+                field: "tags"
+            }
+        }
+    };
+
+    /* Build countNewNotesQuery */
+    let countNewNotesQuery;
+    if ((createdBefore !== null) || (createdAfter !== null)) {
+        countNewNotesQuery = structuredClone(baseQuery);
+        countNewNotesQuery.body.query.bool.must.push({
+            range: {
+                created_at: {
+                    gte: createdBefore || undefined,
+                    gt: createdAfter || undefined
+                }
+            }
+        });
+   }
+
+   /* Build countOldNotesQuery */
+   const countOldNotesQuery = structuredClone(baseQuery);
+   countOldNotesQuery.body.query.bool.must.push({
+        range: {
+            created_at: {
+                lt: createdBefore || undefined,
+                gt: createdAfter || undefined
+            }
+        }
+   });
+
+   const [ notesResult, aggsResult, countNewNotesResult, countOldNotesResult ] = await Promise.all([
+        esClient.search(currentPageResultQuery),
+        esClient.search(aggsResultQuery),
+        (
+            (countNewNotesQuery)
+                ? esClient.count(countNewNotesQuery)
+                : { count: 0 }
+        ),
+        esClient.count(countOldNotesQuery),
+   ]);
+
+   let notes = notesResult.hits.hits;
+   if (createdAfter !== null) {
+        notes = notes.reverse();
+   }
+
+   const countNewNotes = (
+        (createdAfter === null)
+                ? countNewNotesResult?.count || 0
+                : (
+                    (notesByPage >= countNewNotesResult.count)
+                        ? 0
+                        : countNewNotesResult.count - notesByPage
+                )
+   );
+
+   const countOldNotes = (
+        (createdAfter === null)
+            ? (
+                (notesByPage >= countOldNotesResult.count)
+                    ? 0
+                    : countOldNotesResult.count - notesByPage
+            )
+            : countOldNotesResult?.count || 0
+    );
+
+   return {
+        totalNotesInAllPages: (
+            notesResult.hits.total.value + countNewNotes + countOldNotes
+        ),
+        countNewNotes: countNewNotes,
+        countOldNotes: countOldNotes,
+        firstNote:notes[0], 
+        lastNote: notes.at(-1),
+        notesByDay: groupByDay(notes),
+        tags: (aggsResult?.aggregations?.tags_count?.buckets || []).filter(
+            (tag) => {
+                return !tags.includes(tag.key);
+            }
+        )
+   };
+}
---
+++ src/routes/diaries/+page.svelte
@@ -0,0 +1,149 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+    import { format } from "date-fns";
+    import AddTag from "$lib/AddTag.svelte";
+    import RemoveTag from "$lib/RemoveTag.svelte";
+    import TagsFilterList from "$lib/TagsFilterList.svelte";
+    export let data;
+
+    function setUrlHash(value) {
+        const url = new URL(window.location.href);
+        url.hash = value;
+        goto(url.pathname + url.search + url.hash, { replaceState: true });
+    }
+
+    $: displayMoreTags = $page.url.hash === '#display-more-tags';
+
+    $: currentUrl = $page.url;
+
+    let previousPageUrl = "";
+    $: {
+
+        if (data.firstNote) {
+            const url = new URL($page.url);
+            url.searchParams.delete("created_before");
+            url.searchParams.set("created_after", data.firstNote._source.created_at);
+            previousPageUrl = url.toString();
+        }
+    }
+
+    let nextPageUrl = "";
+    $: {
+        if (data.lastNote) {
+            const url = new URL($page.url);
+            url.searchParams.delete("created_after");
+            url.searchParams.set("created_before", data.lastNote._source.created_at);
+            nextPageUrl = url.toString();
+        }
+    }
+
+    let currentFilterTags;
+    $: currentFilterTags = $page.url.searchParams.getAll("tags");
+</script>
+
+<div style="margin: 1em 0">
+    <ul class="current-filter-tags">
+        {#each currentFilterTags as tag}
+            <li>
+                <RemoveTag tag={{key: tag}} currentUrl={currentUrl} />
+            </li>
+        {/each}
+    </ul>
+</div>
+
+{#if data.tags.length > 0}
+    <p>Cliquez sur un tag pour affiner votre recherche :</p>
+
+    <TagsFilterList
+        items={data.tags}
+        let:item={item}
+        expanded={displayMoreTags}
+    >
+        <AddTag tag={item} currentUrl={currentUrl} />
+        <a
+            slot="display-more-tags-button"
+            href="#display-more-tags"
+            on:click={() => { setUrlHash("display-more-tags"); }}
+        >Afficher plus de tags…</a>
+        <a
+            slot="display-less-tags-button"
+            href=""
+            on:click={() => { setUrlHash(""); }}
+        >Afficher moins de tags…</a>
+    </TagsFilterList>
+{/if}
+
+{#if data.countNotes == 0}
+    <p>Aucune note trouvée pour votre recherche.</p>
+{:else}
+    {#if (data.countNewNotes === 0)}
+        <p style="margin-top: 2em">Résultat de la recherche ({data.totalNotesInAllPages} notes) :</p>
+    {:else}
+        <p style="text-align: center">
+            [ <a href={previousPageUrl}>&lt;&lt; Notes plus récentes ({data.countNewNotes})</a> ]
+                {#if (data.countOldNotes === 0)}
+                    Pas de notes plus anciennes
+                {:else}
+                    [ <a href={nextPageUrl}>Notes plus anciennes ({data.countOldNotes}) &gt;&gt; </a> ]
+                {/if}
+        </p>
+    {/if}
+    {#each Object.entries(data.notesByDay) as [date, notes]}
+        <h2>{date}</h2>
+        {#each notes as note}
+
+            {@html note._source.content_html}
+
+            <p>
+                <a href={`/${note._source.filename}/`} rel="bookmark">#</a>
+                {format(note._source.created_at, "HH:mm")}
+                -
+                {#each note._source.tags || [] as tag, i }
+                    {#if i > 0}, {/if}
+                    <a href={`/search/tags=${tag}`}>{tag}</a>
+                {/each}
+            </p>
+            <hr />
+        {/each}
+    {/each}
+    {#if (data.countOldNotes === 0)}
+        <p style="text-align: center">
+            Fin de la liste des notes.
+        </p>
+    {:else}
+        <p style="text-align: center">
+            {#if (data.countNewNotes === 0)}
+                Pas de notes plus récentes
+            {:else}
+            [ <a href={previousPageUrl}>&lt;&lt; Notes plus récentes ({data.countNewNotes})</a> ]
+            {/if}
+            |
+            [ <a href={nextPageUrl}>Notes plus anciennes ({data.countOldNotes}) &gt;&gt; </a> ]
+        </p>
+    {/if}
+{/if}
+<style lang="postcss">
+.current-filter-tags {
+    list-style: none;
+    margin-top: 0.5em;
+    padding: 0;
+    display: flex;
+    gap: 0.5em;
+    flex-wrap: wrap;
+    font-size: 0.7em;
+
+    > LI {
+        display: inline-block;
+        padding: 0.2em 0;
+        border: 1px solid transparent;
+        padding: 0.2em 0.4em;
+        border-color: #aaa;
+
+        > A {
+            white-space: nowrap;
+            text-decoration: none;
+        }
+    }
+}
+</style>
