fc9eec01058b7a6859ad49ba67a214e1e9f0a1c3
stephane-klein <contact@stephane-klein.info>
2024-08-31 19:09:49 +0200

    Add noteType support in search page and fix pagination bugs

--- src/routes/search/+page.server.js
+++ src/routes/search/+page.server.js
@@ -2,6 +2,11 @@ import esClient from "$lib/server/elasticsearch.js";
 const notesByPage = 50;
 
 export async function load({url}) {
+    const tags = url.searchParams.getAll("tags");
+    const queryString = (decodeURIComponent(url.searchParams.get("q") || "")).trim().replace(/"/g, '\\"');
+    const noteType = url.searchParams.get("note_type") || "";
+    const page = parseInt(url.searchParams.get("page") || 1, 10);
+
     /* Build baseQuery used by all other queries */
     const baseQuery = {
         index: "notes",
@@ -9,17 +14,11 @@ export async function load({url}) {
             query: {
                 bool: {
                     must: [
-                        {
-                            term: {
-                                note_type: "journal_note"
-                            }
-                        },
                     ]
                 }
             }
         }
     };
-    const tags = url.searchParams.getAll("tags");
 
     if (tags.length > 0) {
         baseQuery.body.query.bool.must.push(
@@ -35,7 +34,6 @@ export async function load({url}) {
         );
     }
 
-    const queryString = (decodeURIComponent(url.searchParams.get("q") || "")).trim().replace(/"/g, '\\"');
     if (queryString !== "") {
         baseQuery.body.query.bool.must.push({
             query_string: {
@@ -45,24 +43,12 @@ export async function load({url}) {
         });
     }
 
-    const createdAfter = url.searchParams.get("created_after");
-    const createdBefore = url.searchParams.get("created_before");
-
     /* Build currentPageResultQuery */
     const currentPageQuery = structuredClone(baseQuery);
     currentPageQuery.body._source = ["title", "created_at", "filename", "content_html", "tags"];
+    currentPageQuery.body.from = notesByPage * (page - 1);
     currentPageQuery.body.size = notesByPage;
 
-    currentPageQuery.body.query.bool.must.push(
-        {
-            range: {
-                created_at: {
-                    lt: createdBefore || undefined,
-                    gt: createdAfter || undefined
-                }
-            }
-        }
-    );
     if (queryString !== "") {
         currentPageQuery.body._source.splice(currentPageQuery.body._source.indexOf("content_html"), 1);
         currentPageQuery.body.highlight = {
@@ -76,11 +62,18 @@ export async function load({url}) {
             }
         };
     }
+    if (noteType !== "") {
+        currentPageQuery.body.query.bool.must.push({
+            term: {
+                note_type: noteType
+            }
+        });
+    }
 
-    /* Build aggsResultQuery*/
-    const aggsQuery = structuredClone(baseQuery);
-    aggsQuery.body.size = 0;
-    aggsQuery.body.aggs = {
+    /* Build aggsTagsResultQuery*/
+    const aggsTagsQuery = structuredClone(baseQuery);
+    aggsTagsQuery.body.size = 0;
+    aggsTagsQuery.body.aggs = {
         tags_count: {
             terms: {
                 field: "tags",
@@ -88,81 +81,52 @@ export async function load({url}) {
             }
         }
     };
-
-    /* Build countNewNotesQuery */
-    let countNewNotesQuery;
-    if ((createdBefore !== null) || (createdAfter !== null)) {
-        countNewNotesQuery = structuredClone(baseQuery);
-        countNewNotesQuery.body.query.bool.must.push({
-            range: {
-                created_at: {
-                    gte: createdBefore || undefined,
-                    gt: createdAfter || undefined
-                }
+    if (noteType !== "") {
+        aggsTagsQuery.body.query.bool.must.push({
+            term: {
+                note_type: noteType
             }
         });
-   }
+    }
 
-   /* Build countOldNotesQuery */
-   const countOldNotesQuery = structuredClone(baseQuery);
-   countOldNotesQuery.body.query.bool.must.push({
-        range: {
-            created_at: {
-                lt: createdBefore || undefined,
-                gt: createdAfter || undefined
+    /* Build aggsNoteTypesResultQuery*/
+    const aggsNoteTypesQuery = structuredClone(baseQuery);
+    aggsNoteTypesQuery.body.size = 0;
+    aggsNoteTypesQuery.body.aggs = {
+        note_type_count: {
+            terms: {
+                field: "note_type",
+                size: 10000
             }
         }
-   });
+    };
 
-   const [ notesResult, aggsResult, countNewNotesResult, countOldNotesResult ] = await Promise.all([
+   const [ notesResult, aggsTagsResult, aggsNoteTypesResult] = await Promise.all([
         esClient.search(currentPageQuery),
-        esClient.search(aggsQuery),
-        (
-            (countNewNotesQuery)
-                ? esClient.count(countNewNotesQuery)
-                : { count: 0 }
-        ),
-        esClient.count(countOldNotesQuery),
+        esClient.search(aggsTagsQuery),
+        esClient.search(aggsNoteTypesQuery),
    ]);
 
    let notes = notesResult.hits.hits;
-   if (createdAfter !== null) {
-        notes = notes.reverse();
-   }
 
-   const countNewNotes = (
-        (createdAfter === null)
-                ? countNewNotesResult?.count || 0
-                : (
-                    (notesByPage >= countNewNotesResult.count)
-                        ? 0
-                        : countNewNotesResult.count - notesByPage
-                )
-   );
+   let countNotesInPreviousPages = (page - 1) * notesByPage;
+   if (countNotesInPreviousPages < 0) countNotesInPreviousPages = 0;
 
-   const countOldNotes = (
-        (createdAfter === null)
-            ? (
-                (notesByPage >= countOldNotesResult.count)
-                    ? 0
-                    : countOldNotesResult.count - notesByPage
-            )
-            : countOldNotesResult?.count || 0
-   );
+   let countNotesInNextPages = notesResult.hits.total.value - (page * notesByPage);
+   if (countNotesInNextPages < 0) countNotesInNextPages = 0;
 
    return {
         totalNotesInAllPages: (
-            notesResult.hits.total.value + countNewNotes + countOldNotes
+            notesResult.hits.total.value
         ),
-        countNewNotes: countNewNotes,
-        countOldNotes: countOldNotes,
-        firstNote:notes[0], 
-        lastNote: notes.at(-1),
+        countNotesInPreviousPages: countNotesInPreviousPages,
+        countNotesInNextPages: countNotesInNextPages,
         notes: notes,
-        tags: (aggsResult?.aggregations?.tags_count?.buckets || []).filter(
+        tags: (aggsTagsResult?.aggregations?.tags_count?.buckets || []).filter(
             (tag) => {
                 return !tags.includes(tag.key);
             }
-        )
+        ),
+        noteTypes: (aggsNoteTypesResult?.aggregations?.note_type_count?.buckets || [])
    };
 }
