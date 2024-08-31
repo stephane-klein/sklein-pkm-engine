1d344e9326952800f1b1052362f143945dfb1ce0
stephane-klein <contact@stephane-klein.info>
2024-08-31 12:06:26 +0200

    Add /notes/ page

---
+++ src/routes/notes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let data;
+</script>
+{#each Object.entries(data.notesGroupedByFirstLetter) as [firstLetter, notes]}
+    <h2>{firstLetter}</h2>
+
+    <ul>
+    {#each notes as note}
+        <li><a href={`/${note._source.filename}/`}>{note._source.title}</a></li>
+    {/each}
+    </ul>
+{/each}
