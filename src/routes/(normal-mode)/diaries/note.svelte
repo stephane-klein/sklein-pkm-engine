3c6b918dde6fc59148b2ef5b3300b4fa17944451
stephane-klein <contact@stephane-klein.info>
2024-09-27 12:58:36 +0200

    Limit note height in note list page and implement a button to display all note content

--- src/routes/(normal-mode)/diaries/note.svelte
+++ src/routes/(normal-mode)/diaries/note.svelte
@@ -1,34 +1,64 @@
 <script>
+    import { browser } from '$app/environment';
+    import clsx from "clsx/lite";
     import { format } from "date-fns";
+    import { fr } from "date-fns/locale";
+    import { onMount } from "svelte";
 
     export let filename;
     export let title;
     export let created_at;
     export let tags;
     export let content_html;
+
+    let bodyNode;
+    let overflowed;
+    let maxHeight;
+    let close = true;
+
+    onMount(() => {
+        if (browser) {
+            overflowed = (bodyNode.scrollHeight > bodyNode.clientHeight);
+            maxHeight = bodyNode.scrollHeight;
+        }
+    });
+
 </script>
 <div class="journal-note">
     <p class="header">
-        <span class="note-datetime">
+        <span class="note-title">
             <a
                 href={`/${filename}/`}
             >
                 {#if title}
                     {title}
                 {:else}
-                    Journal du {format(created_at, "yyyy-MM-dd à HH:mm")}
+                    Journal du {format(created_at, "eeee dd MMMM yyyy à HH:mm", {locale: fr})}
                 {/if}
             </a>
         </span>
 
         <span class="tags">
-            {#each tags || [] as tag }
-                <a href={`/search/tags=${tag}`}>#{tag}</a>
+            {#each tags || [] as tag, i }
+                <span><a href={`/search/tags=${tag}`}>#{tag}</a>{#if i < tags.length - 1}, {/if}</span>
             {/each}
         </span>
     </p>
 
-    <div class="body">
-    {@html content_html}
+    <div
+        class={clsx("body", close && "close")}
+        style:max-height={!close ? `${maxHeight + 100}px` : null}
+        bind:this={bodyNode}
+    >
+        {@html content_html}
     </div>
+    {#if overflowed && close}
+        <div class="see-more-or-less">
+            <button on:click={() => {close = false; }}>Afficher l'intégralité de la note</button>
+        </div>
+    {:else if (!close)}
+        <div class="see-more-or-less">
+            <button on:click={() => {close = true; }}>Réduire l'affichage de la note</button>
+        </div>
+    {/if}
 </div>
