21f6e43abf9cf1dca81a98279a6a50ac91a7a5e4
stephane-klein <contact@stephane-klein.info>
2024-10-03 12:19:19 +0200

    Integration of listr in note import script

--- import-to-es-database.js
+++ import-to-es-database.js
@@ -5,189 +5,252 @@ import { glob } from "glob";
 import { Client } from "@elastic/elasticsearch";
 import matter from "gray-matter";
 import yaml from "js-yaml";
+import { Listr } from "listr2";
 import { extractLinksAndTags } from "./utils.js";
 import md from "./src/lib/server/markdown.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+process.chdir(__dirname);
+
 const client = new Client({
     node: process.env.ELASTICSEARCH_URL || "http://localhost:9200"
 });
 
-if (!await client.indices.exists({ index: "notes" })) {
+let ctx = {
+    notesIndiceExists: false,
+    allNoteFileNames: [],
+    contentAbsPath: path.resolve(".", process.env.CONTENT_PATH || "content/"),
+    allFilesToImport: []
+};
 
-    // If you modify this data model, you must delete the database with the following script:
-    //
-    // $ ./reset-es-database.js
-    //
-    await client.indices.create({
-        index: "notes",
-        body: {
-            settings: {
-                analysis: {
-                    analyzer: {
-                        french_analyzer: {
-                            type: "custom",
-                            tokenizer: "standard",
-                            filter: [
-                                "lowercase",
-                                "asciifolding",
-                                "french_elision",
-                                "french_stop",
-                                "french_stemmer"
-                            ]
+const tasks = new Listr(
+    [
+        {
+            title: `Check if "notes" indice exists in Elasticsearch database`,
+            task: async(ctx, task) => {
+                ctx.notesIndiceExists = await client.indices.exists({ index: "notes" });
+                if (ctx.notesIndiceExists) {
+                    task.title = `Check if "notes" indice exists in Elasticsearch database: Yes`;
+                } else {
+                    task.title = `Check if "notes" indice exists in Elasticsearch database: No`;
+                }
+            }
+        },
+        {
+            title: `Create "notes" indice`,
+            skip: (ctx) => ctx.notesIndiceExists,
+            task: async() => {
+                // If you modify this data model, you must delete the database with the following script:
+                //
+                // $ ./reset-es-database.js
+                //
+                await client.indices.create({
+                    index: "notes",
+                    body: {
+                        settings: {
+                            analysis: {
+                                analyzer: {
+                                    french_analyzer: {
+                                        type: "custom",
+                                        tokenizer: "standard",
+                                        filter: [
+                                            "lowercase",
+                                            "asciifolding",
+                                            "french_elision",
+                                            "french_stop",
+                                            "french_stemmer"
+                                        ]
+                                    },
+                                    french_html_analyzer: {
+                                        type: "custom",
+                                        tokenizer: "standard",
+                                        filter: [
+                                            "lowercase",
+                                            "asciifolding",
+                                            "french_elision",
+                                            "french_stop",
+                                            "french_stemmer"
+                                        ],
+                                        char_filter: [
+                                            "html_strip"
+                                        ]
+                                    }
+                                },
+                                filter: {
+                                    french_elision: {
+                                        type: "elision",
+                                        articles_case: true,
+                                        articles: [
+                                            "l", "m", "t", "qu", "n", "s", "j", "d", "c", "jusqu", "quoiqu", "lorsqu", "puisqu"
+                                        ]
+                                    },
+                                    french_stop: {
+                                        type: "stop",
+                                        stopwords: "_french_"
+                                    },
+                                    french_stemmer: {
+                                        type: "stemmer",
+                                        language: "light_french"
+                                    }
+                                }
+                            }
                         },
-                        french_html_analyzer: {
-                            type: "custom",
-                            tokenizer: "standard",
-                            filter: [
-                                "lowercase",
-                                "asciifolding",
-                                "french_elision",
-                                "french_stop",
-                                "french_stemmer"
-                            ],
-                            char_filter: [
-                                "html_strip"
-                            ]
+                        mappings: {
+                            properties: {
+                                title: {
+                                    type: "text",
+                                    analyzer: "french_analyzer",
+                                    fields: {
+                                        keyword: {
+                                            type: "keyword"
+                                        }
+                                    }
+                                },
+                                filename: {
+                                    type: "keyword"
+                                },
+                                created_at: {
+                                    type: "date",
+                                    format: "yyyy-MM-dd HH:mm:ss"
+                                },
+                                note_type: {
+                                    type: "keyword"
+                                },
+                                linked_notes: {
+                                    type: "keyword"
+                                },
+                                tags: {
+                                    type: "keyword"
+                                },
+                                content: {
+                                    type: "text",
+                                    analyzer: "french_analyzer"
+                                },
+                                content_html: {
+                                    type: "text",
+                                    analyzer: "french_html_analyzer"
+                                }
+                            }
                         }
-                    },
-                    filter: {
-                        french_elision: {
-                            type: "elision",
-                            articles_case: true,
-                            articles: [
-                                "l", "m", "t", "qu", "n", "s", "j", "d", "c", "jusqu", "quoiqu", "lorsqu", "puisqu"
-                            ]
+                    }
+                });
+            }
+        },
+        {
+            title: "Loads the names of all notes already in the Elasticsearch database",
+            task: async(ctx) => {
+                // List used later to delete all notes that no longer exist
+                ctx.allNoteFileNames = (
+                    await client.search({
+                        index: "notes",
+                        body: {
+                            query: {
+                                match_all: {}
+                            },
+                            stored_fields: [],
+                            _source: false
                         },
-                        french_stop: {
-                            type: "stop",
-                            stopwords: "_french_"
-                        },
-                        french_stemmer: {
-                            type: "stemmer",
-                            language: "light_french"
+                        size: 10000
+                    })
+                ).hits.hits.map((row) => row._id);
+            }
+        },
+        {
+            title: "Loads a list of all note files to be processed",
+            task: async(ctx) => {
+                ctx.allFilesToImport = await glob(
+                    "/src/**/*.md",
+                    {
+                        cwd: ctx.contentAbsPath,
+                        root: ctx.contentAbsPath,
+                        dot: false,
+                        ignore: [
+                            "src/Templates/**",
+                            "src/attachments/**"
+                        ]
+                    }
+                );
+            }
+        },
+        {
+            title: "Upload notes from filesystem to database",
+            task: async(ctx, task) => {
+                let index = 0;
+                const contentAbsPathLength = ctx.contentAbsPath.length;
+                for await (const filePath of ctx.allFilesToImport) {
+                    index++;
+                    task.title = `Upload notes from filesystem to database (${index}/${ctx.allFilesToImport.length})`;
+                    const fileName = path.parse(path.basename(filePath)).name;
+                    const relativeFilePath = filePath.substring(contentAbsPathLength);
+
+                    const data = matter.read(filePath, {
+                        engines: {
+                            yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA })
                         }
+                    });
+                    if (
+                        (data.data.draft === true) ||
+                            (data.data.draft === "true")
+                    ) {
+                        task.output = `Skip draft note: ${relativeFilePath}`;
+                        continue;
+                    } else {
+                        task.output = `Load ${relativeFilePath} to database`;
+                    }
+
+                    const [WikiLinks, Tags] = extractLinksAndTags(data.content);
+                    data.data.tags = [...new Set([...data.data?.tags || [], ...Tags])];
+
+                    await client.index({
+                        index: "notes",
+                        id: fileName,
+                        document: {
+                            filename: fileName,
+                            created_at: (data.data.created_at && (data.data.type === "journal_note")) ? data.data.created_at + ":00" : null,
+                            title: data.data?.title || ((data.data.type !== "journal_note") ? path.parse(fileName).name : undefined),
+                            note_type: data.data?.type || null,
+                            linked_notes: WikiLinks,
+                            tags: data.data?.tags || [],
+                            content: data.content,
+                            content_html: md.render(data.content)
+                        },
+                    });
+                    const fileNameIndex = ctx.allNoteFileNames.findIndex((item) => item === fileName);
+                    if (fileNameIndex > -1) {
+                        ctx.allNoteFileNames.splice(fileNameIndex, 1);
                     }
                 }
             },
-            mappings: {
-                properties: {
-                    title: {
-                        type: "text",
-                        analyzer: "french_analyzer",
-                        fields: {
-                            keyword: {
-                                type: "keyword"
-                            }
-                        }
-                    },
-                    filename: {
-                        type: "keyword"
-                    },
-                    created_at: {
-                        type: "date",
-                        format: "yyyy-MM-dd HH:mm:ss"
-                    },
-                    note_type: {
-                        type: "keyword"
-                    },
-                    linked_notes: {
-                        type: "keyword"
-                    },
-                    tags: {
-                        type: "keyword"
-                    },
-                    content: {
-                        type: "text",
-                        analyzer: "french_analyzer"
-                    },
-                    content_html: {
-                        type: "text",
-                        analyzer: "french_html_analyzer"
-                    }
+        },
+        {
+            title: "Delete any notes still present in the database but which have been deleted from the filesystem",
+            task: async(ctx, task) => {
+                let index = 0;
+                task.title = `Delete any notes still present in the database but which have been deleted from the filesystem (${index}/${ctx.allNoteFileNames.length})`;
+                for (const fileName of ctx.allNoteFileNames) {
+                    index++;
+                    task.title = `Delete any notes still present in the database but which have been deleted from the filesystem (${index}/${ctx.allNoteFileNames.length})`;
+                    task.output = `Delete "${fileName}" note from database`;
+                    await client.delete({
+                        index: "notes",
+                        id: fileName
+                    });
                 }
             }
         }
-    });
-}
-
-// List used later to delete all notes that no longer exist
-const allNoteFileNames = (
-    await client.search({
-        index: "notes",
-        body: {
-            query: {
-                match_all: {}
-            },
-            stored_fields: [],
-            _source: false
-        },
-        size: 10000
-    })
-).hits.hits.map((row) => row._id);
-
-process.chdir(__dirname);
-
-const contentAbsPath = path.resolve(".", process.env.CONTENT_PATH || "content/");
-
-for await (const filePath of (await glob(
-    "/src/**/*.md",
+    ],
     {
-        cwd: contentAbsPath,
-        root: contentAbsPath,
-        dot: false,
-        ignore: [
-            "src/Templates/**",
-            "src/attachments/**"
-        ]
-    }
-))) {
-    const data = matter.read(filePath, {
-        engines: {
-            yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA })
+        ctx,
+        rendererOptions: {
+            suffixSkips: true
         }
-    });
-    console.log(`Import ${path.relative(contentAbsPath, filePath)}`);
-    if (
-        (data.data.draft === true) ||
-        (data.data.draft === "true")
-    ) {
-        console.log("Skip draft note");
-        continue;
     }
+);
 
-    const [WikiLinks, Tags] = extractLinksAndTags(data.content);
-    data.data.tags = [...new Set([...data.data?.tags || [], ...Tags])];
-
-    const fileName = path.parse(path.basename(filePath)).name;
-
-    await client.index({
-        index: "notes",
-        id: fileName,
-        document: {
-            filename: fileName,
-            created_at: (data.data.created_at && (data.data.type === "journal_note")) ? data.data.created_at + ":00" : null,
-            title: data.data?.title || ((data.data.type !== "journal_note") ? path.parse(fileName).name : undefined),
-            note_type: data.data?.type || null,
-            linked_notes: WikiLinks,
-            tags: data.data?.tags || [],
-            content: data.content,
-            content_html: md.render(data.content)
-        },
-    });
-    const fileNameIndex = allNoteFileNames.findIndex((item) => item === fileName);
-    if (fileNameIndex > -1) {
-        allNoteFileNames.splice(fileNameIndex, 1);
-    }
-}
-
-// Delete all notes that no longer exist
-for (const fileName of allNoteFileNames) {
-    console.log(`Delete "${fileName}" note still in Elastic Search database`);
-    await client.delete({
-      index: "notes",
-      id: fileName
-    });
+try {
+    await tasks.run();
+} catch (e) {
+    console.error(e);
 }
