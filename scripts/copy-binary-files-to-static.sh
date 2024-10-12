eb11bb7ac66208b52e0cc83a2fa04ac22b5a698e
stephane-klein <contact@stephane-klein.info>
2024-10-12 12:31:53 +0200

    Replace move to copy binary files to static

---
+++ scripts/copy-binary-files-to-static.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname "$0")/../"
+
+rm -rf static
+mkdir static
+find "${CONTENT_PATH:-'content'}/src/" -type f ! -name "*.md" -exec cp {} "static/" \;
--- scripts/move-binary-files-to-static.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd "$(dirname "$0")/../"
-
-rm -rf static
-mkdir static
-find "${CONTENT_PATH:-'content'}/src/" -type f ! -name "*.md" -exec mv {} "static/" \;
