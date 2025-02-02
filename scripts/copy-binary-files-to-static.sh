d2d60972c29ccaa32ae5040a63687249d324470b
stephane-klein <contact@stephane-klein.info>
2025-02-02 18:49:33 +0100

    Fix bug

--- scripts/copy-binary-files-to-static.sh
+++ scripts/copy-binary-files-to-static.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")/../"
 
 rm -rf static
 mkdir static
-find "${CONTENT_PATH:-'content'}/src/" -type f \( -name "*.gif" -o -name "*.png" -o -name "*.jpg" \) -exec cp {} "static/" \;
+find "${CONTENT_PATH:-'content'}/src/" -type f \( -name "*.jpeg" -o -name "*.gif" -o -name "*.png" -o -name "*.jpg" \) -exec cp {} "static/" \;
 cp assets/* static/
