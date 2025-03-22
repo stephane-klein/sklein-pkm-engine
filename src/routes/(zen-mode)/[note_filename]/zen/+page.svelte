fd484c3f62c0427e0fefe8cb50a0d0e6e14fd4af
stephane-klein <contact@stephane-klein.info>
2025-03-22 13:27:49 +0100

    Remove Avatar in unlisted notes

--- src/routes/(zen-mode)/[note_filename]/zen/+page.svelte
+++ src/routes/(zen-mode)/[note_filename]/zen/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import { format } from "date-fns";
     import { fr } from "date-fns/locale";
+    import "../../../../screen.css";
+    import Avatar from "$lib/assets/favicon-48x48.png";
     
     function formatDate(value) {
         const formattedDate = format(value, "eeee dd MMMM yyyy à HH:mm", {locale: fr});
@@ -9,29 +11,34 @@
 
     export let data;
 </script>
-<div class="note">
-    {#if (data.note._source.note_type !== "unlisted") || (data.note._source?.title)}
-        <h1>{data.note._source?.title || formatDate(data.note._source.created_at)}</h1>
+<div class="zen-mode">
+    {#if data.note._source.note_type !== "unlisted"}
+        <a href="/"><img src={Avatar} alt="Logo" class="logo" /></a>
     {/if}
+    <div class="note">
+        {#if (data.note._source.note_type !== "unlisted") || (data.note._source?.title)}
+            <h1>{data.note._source?.title || formatDate(data.note._source.created_at)}</h1>
+        {/if}
 
-    {#if data.note._source.tags.length > 0}
-        <ul class="tags">
-            {#each data.note._source.tags as tag, i}
-                <li><a href={`/search/?tags=${tag}`}>#{tag}</a>{#if i < data.note._source.tags.length - 1}, {/if}
-                </li>
-            {/each}
-        </ul>
-    {/if}
+        {#if data.note._source.tags.length > 0}
+            <ul class="tags">
+                {#each data.note._source.tags as tag, i}
+                    <li><a href={`/search/?tags=${tag}`}>#{tag}</a>{#if i < data.note._source.tags.length - 1}, {/if}
+                    </li>
+                {/each}
+            </ul>
+        {/if}
 
-    {#if (data.note._source.note_type === "journal_note") && data.note._source?.title}
-        <p class="datetime">Journal du {formatDate(data.note._source.created_at).toLowerCase()}</p>
-    {/if}
+        {#if (data.note._source.note_type === "journal_note") && data.note._source?.title}
+            <p class="datetime">Journal du {formatDate(data.note._source.created_at).toLowerCase()}</p>
+        {/if}
 
-    <div class="body">
-        {@html data.note._source.content_html}
+        <div class="body">
+            {@html data.note._source.content_html}
+        </div>
     </div>
-</div>
 
-{#if data.note._source.note_type !== "unlisted"}
-    <p style="text-align: center;margin-top: 5rem;"><a href="../" class="action-button">Quitter le mode Zen</a></p>
-{/if}
+    {#if data.note._source.note_type !== "unlisted"}
+        <p style="text-align: center;margin-top: 5rem;"><a href="../" class="action-button">Quitter le mode Zen</a></p>
+    {/if}
+</div>
